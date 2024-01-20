fbf4fb6812e99661c11ed4b89850cbcef47e25ed
Christof Becu <christof.becu@gmail.com>
2024-01-20 11:22:01 +0100

    solve 7kyu number of people in the bus

---
+++ codewars/7kyu/number_of_people_in_the_bus/src/lib.rs
@@ -0,0 +1,14 @@
+fn number(bus_stops:&[(i32,i32)]) -> i32 {
+    let mut result: i32 = 0;
+    for (on, off) in bus_stops {
+        result += on - off;
+    }
+    result
+}
+
+#[test]
+fn returns_expected() {
+    assert_eq!(number(&[(10,0),(3,5),(5,8)]), 5);
+    assert_eq!(number(&[(3,0),(9,1),(4,10),(12,2),(6,1),(7,10)]), 17);
+    assert_eq!(number(&[(3,0),(9,1),(4,8),(12,2),(6,1),(7,8)]), 21);
+}
